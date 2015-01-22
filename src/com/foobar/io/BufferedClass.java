c9bff77a0a4eec3b01cdbd1d6476538ccf11ec14
atcuan <atcuan@gmail.com>
2015-01-22 23:15:37 +0800

    io

---
+++ src/com/foobar/io/BufferedClass.java
@@ -0,0 +1,99 @@
+package com.foobar.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.junit.Test;
+
+public class BufferedClass {
+
+	public BufferedClass() {
+	}
+
+	@Test
+	public void testBufferedInputAndOutputStream() {
+		File inFile = new File("Objc.md");
+		File outFile = new File("ObjcCp.md");
+
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		
+		try {
+			fis = new FileInputStream(inFile);
+			fos = new FileOutputStream(outFile);
+			
+			bis = new BufferedInputStream(fis);
+			bos = new BufferedOutputStream(fos);
+			
+			byte[] by = new byte[24];
+			int len;
+			for (; -1 != (len = bis.read(by));) {
+				bos.write(by, 0, len);
+				bos.flush();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testBufferedInputAndOutputStream2() {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(new File("Objc.md")));
+			bw = new BufferedWriter(new FileWriter(new File("Objc_readLine.md")));
+			
+			String line;
+			for (; null != (line = br.readLine());) {
+				bw.write(line);
+				bw.newLine();
+				bw.flush();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bw != null) {
+				try {
+					bw.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (br != null) {
+				try {
+					br.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
---
+++ src/com/foobar/io/IOStream.java
@@ -0,0 +1,77 @@
+package com.foobar.io;
+
+import org.junit.Test;
+
+// 文件流
+// FileInputStream FileOutputStream FileReader FileWriter
+
+// 缓冲流
+// BufferedInputStream BufferedOuputStream BufferedReader BufferedWriter
+
+// 转换流
+// InputStreamReader OutputStreamWriter
+
+// 打印流
+// PrintStream PrintWriter
+
+// 数据流
+// DataInputStream DataOutputStream
+
+// 对象流
+// ObjectInputStream ObjectOutputStream
+
+// 随机存取文件流
+// RandomAccessFile
+
+public class IOStream {
+	private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	
+	public IOStream() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println(FILE_SEPARATOR);
+		System.out.println(System.getProperties());
+	}
+
+	@Test
+	public void testFileStream() {
+		
+	}
+	
+	@Test
+	public void testBufferedStream() {
+		
+	}
+	
+	@Test
+	public void testIOPutStream() {
+		
+	}
+	
+	@Test
+	public void testPrintStream() {
+		
+	}
+	
+	@Test
+	public void testDataStream() {
+
+	}
+	
+	@Test 
+	public void testObjectStream() {
+		
+	}
+	
+	@Test
+	public void testRandomAccessFileStream() {
+		
+	}
+}
+
+
+
+
+
+
